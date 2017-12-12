3ac24d07a457f3bb9fb2d33498498de4df9a3586
nathan lachenmyer <nathan@midnightcommercial.com>
2017-12-12 16:12:57 -0500

    first draft of chocolatey files

---
+++ scripts/install_chocolatey_development.ps1
@@ -0,0 +1,37 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+chocolatey feature enable -n allowGlobalConfirmation
+
+# web browsers
+choco install googlechrome
+
+# video
+choco install vlc
+choco install quicktime
+
+# text editors
+choco install atom
+
+# useful for production
+choco install git
+choco install sourcetree
+choco install visualstudio2017community
+choco install meteor
+choco install python --version 3.5.4
+choco install pip
+
+# useful for development
+choco install nodejs
+choco install arduino
+choco install cmake
+choco install kicad
+choco install ffmpeg
+
+# misc windows stuff
+choco install autohotkey.portable
+choco install 7zip
+choco install filezilla
+choco install teamviewer
+choco install wireshark
+
+refreshenv
---
+++ scripts/install_chocolatey_production.ps1
@@ -0,0 +1,36 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+chocolatey feature enable -n allowGlobalConfirmation
+
+# web browsers
+choco install googlechrome
+
+# video
+choco install vlc
+choco install quicktime
+
+# text editors
+choco install atom
+
+# for cloning repos
+choco install git
+choco install sourcetree
+
+# for running C++ executables
+choco install vcredist2017
+
+# for running python scripts / downloading requirements
+choco install python --version 3.5.4
+choco install pip
+
+# for running node programs
+choco install nodejs
+
+# misc windows stuff
+choco install autohotkey.portable
+choco install 7zip
+choco install filezilla
+choco install teamviewer
+choco install wireshark
+
+refreshenv
